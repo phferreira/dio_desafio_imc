0616d5d070ab9e7d0c10b1158d3480cb369c8353
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-09-29 12:30:53 -0300

    feat: Call PersonPage for main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio_desafio_imc/view/person_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: PersonPage(),
       ),
     );
   }

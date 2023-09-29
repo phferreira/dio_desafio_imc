78644b07104b379573b8b29075f9778fe35df117
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-09-29 11:05:43 -0300

    fix: IMC validations
    - Add corrects messages

--- lib/controller/person_controller.dart
+++ lib/controller/person_controller.dart
@@ -8,19 +8,29 @@ class PersonController extends ValueNotifier<double> {
 
   calculateImc(Person person) {
     try {
-      //
-      if ((person.height == 0) || (person.weight == 0)) {
-        // throw Exception();
+      if ((person.height <= 0) || (person.height > 3.0)) {
         value = 0;
-        message = 'Informação invalida!';
+        message = 'Informação invalida para o campo altura!';
+      }
+      if ((person.weight <= 0) || (person.weight > 300)) {
+        value = 0;
+        message = 'Informação invalida para o campo peso!';
       } else {
         final double imc = person.weight / (person.height * person.height);
         value = double.parse(imc.toStringAsFixed(2));
 
-        if ((value >= 15) && (value <= 30)) {
-          message = 'IMC normal.';
-        } else {
+        if (value <= 18.5) {
+          message = 'Baixo peso';
+        } else if ((value > 18.5) && (value < 25)) {
+          message = 'Peso normal';
+        } else if ((value > 25) && (value < 30)) {
           message = 'Sobrepeso';
+        } else if ((value > 30) && (value < 35)) {
+          message = 'Obesidade grau I';
+        } else if ((value > 35) && (value < 40)) {
+          message = 'Obesidade grau II';
+        } else {
+          message = 'Obesidade grau III';
         }
       }
       notifyListeners();

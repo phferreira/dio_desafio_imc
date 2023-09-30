24d7d9bb20114793061ca728c5ce55b5929cae3d
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-09-30 09:29:38 -0300

    feat: Added imc field to store the value

--- lib/model/person.dart
+++ lib/model/person.dart
@@ -2,15 +2,18 @@ class Person {
   String name;
   double weight;
   double height;
+  double imc;
 
   Person({
     required this.name,
     required this.weight,
     required this.height,
+    required this.imc,
   });
 
   Person.empty()
       : name = '',
         weight = 0.0,
-        height = 0.0;
+        height = 0.0,
+        imc = 0.0;
 }
